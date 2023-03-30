0145936de558bdc5d1419a88ab8ca400f708e9ea
kashvigarg <kashvigarg02@gmail.com>
2023-03-30 19:01:05 +0530

    added json response

--- server/views.py
+++ server/views.py
@@ -1,20 +1,12 @@
-from rest_framework.decorators import parser_classes
-from django.core.files.storage import default_storage
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import generics
-from django.http import HttpResponse
-import io, csv, pandas as pd
+import rest_framework.status as status
+import pandas as pd
 import seaborn as sns
-import numpy as np
 from pyod.models.iforest import IForest
-from sklearn.ensemble import IsolationForest
-from datetime import datetime, timedelta 
-from django.shortcuts import render
-from server.models import File
-from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import MultiPartParser, FormParser
+from datetime import timedelta 
 from server.serializers import UploadSerializer
-from . import serializers
 
 class UploadCSV(APIView):
     parser_classes = (MultiPartParser, FormParser)
@@ -26,7 +18,7 @@ class UploadCSV(APIView):
             serializer.save()
             file = serializer.data
             df = pd.read_csv(file, delimiter=';')
-            df = df[['date', 'account_id', 'type', 'amount']]
+            df = df[['trans_id', 'date', 'account_id', 'type', 'amount']]
             df['date'] = pd.to_datetime(df['date'], format='%y%m%d')
             df.replace(to_replace='PRIJEM', value = 'Credit', inplace= True)
             df.replace(to_replace='VYDAJ', value = 'Withdrawl', inplace= True)
@@ -34,7 +26,6 @@ class UploadCSV(APIView):
             df_C = df.query('type == "Credit"')
             df_W = df_W.sort_values(by=['account_id', 'date']).set_index('date')
             df_C = df_C.sort_values(by=['account_id', 'date']).set_index('date')
-            delta = timedelta(days = 5)
             df_W['sum_5days'] = df_W.groupby('account_id')['amount'].transform(lambda s: s.rolling(timedelta(days=5)).sum())
             df_W['freq_5days'] = df_W.groupby('account_id')['amount'].transform(lambda s: s.rolling(timedelta(days=5)).count())
 
@@ -50,10 +41,12 @@ class UploadCSV(APIView):
             df_W['score'] = model.decision_scores_
             
             flagged = df_W.query('pred == 1')
-            jsrec = flagged.reset_index().to_json(orient ='records')
-            arr = []
-            arr = json.loads(jsrec)
+            f_flagged = df[['trans_id', 'pred', 'score']]
+            jsrec = f_flagged.to_json(orient ='records')
+            context = {
+             'data': jsrec,
+            }
             
-            return Response(arr, status=status.HTTP_201_CREATED)
+            return Response(context, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
